44fa27b85053575583a3a10bfffda6087945a8e4
Tin Pham <tinpham.swe@gmail.com>
2026-09-05 21:49:02 +0700

    Initial Commit

---
+++ InboxBrief/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InboxBrief
+//
+//  Created by Tin Pham on 5/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InboxBrief/InboxBriefApp.swift
@@ -0,0 +1,17 @@
+//
+//  InboxBriefApp.swift
+//  InboxBrief
+//
+//  Created by Tin Pham on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct InboxBriefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
